37a156cc548182d632cadaff89da456a520c524e
vagrant <vagrant@precise32.(none)>
2014-02-27 20:26:31 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('.likeBtn').click(sendEvent)
+	function sendEvent (e) {
+		ga("send", "event", "like", "click");
+	}
 }
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var projects = require('../projects.json');
 
+
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
+  	projects ['grid'] = false;
+  	res.render('index', projects);
+};
+
+exports.viewGrid = function(req, res){
+  	projects ['grid'] = true;
   	res.render('index', projects);
 };
